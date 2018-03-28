81014cb8045e152aca6df46798dfde7039dbdb55
AntonAndell <anton@andell.eu>
2018-03-28 07:49:10 +0200

    peer class

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import sys, bencoder, requests, hashlib, random
 from string import ascii_letters, digits
 import ipaddress, struct
+import socket
 VERSION = '0001'
 ALPHANUM = ascii_letters + digits
 DEFAULT_PORT = "55308"
@@ -9,24 +10,25 @@ class Torrent(object):
         self.torrent_dict = self.get_torrent_dict(torrent_path)
         self.peer_addresses = []
         self.port = port
-
-
+        self.peer_id = peer_id = ('-DR' + VERSION + ''.join(random.sample(ALPHANUM, 13)))
     @property
     def torrent_payload(self):
         payload = {}
-        info_hash = hashlib.sha1(bencoder.encode(self.torrent_dict[b'info'])).digest()
-        peer_id = ('-DR' + VERSION + ''.join(random.sample(ALPHANUM, 13)))
-        payload['info_hash'] = info_hash
-        payload['peer_id'] = peer_id
-        payload['port'] = self.port
-        payload['uploaded'] = '0'
+
+        payload['info_hash']  = self.info_hash
+        payload['peer_id']    = self.peer_id
+        payload['port']       = self.port
+        payload['uploaded']   = '0'
         payload['downloaded'] = '0'
-        payload['left'] = str(self.torrent_dict[b'info'][b'length'])
-        payload['compact'] = '1'
-        payload['supportcrypto'] = '1'
-        payload['event'] = 'started'
+        payload['left']       = str(self.torrent_dict[b'info'][b'length'])
+        payload['compact']    = '1'
+        payload['event']      = 'started'
         return payload
 
+    @property
+    def info_hash(self):
+        info_hash = hashlib.sha1(bencoder.encode(self.torrent_dict[b'info'])).digest()
+        return info_hash
     def get_peer_addresses(self):
         tracker_adress = self.torrent_dict[b'announce']
         r = requests.get(tracker_adress, params=self.torrent_payload)
@@ -43,6 +45,34 @@ class Torrent(object):
             torrent = bencoder.decode(fs.read())
         return torrent
 
+class Peer(object):
+    def __init__(self, ip, port, torrent):
+        self.choked = True
+        self.interested = False
+        self.ip = ip
+        self.port = port
+        self.torrent = torrent
+        self.s = None
+    @property
+    def address(self):
+        return (self.ip, (self.port))
+    def get_packet(self):
+        params = []
+        params.append(chr(len("BitTorrent protocol")))
+        params.append("BitTorrent protocol")
+        params.append(str(chr(0)*8))
+        params.append(str(self.torrent.info_hash))
+        params.append(self.torrent.peer_id)
+        return ''.join(params)
+    def handshake(self):
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.setblocking(True)
+        self.s.connect(self.address)
+        self.s.send(self.get_packet())
+        data = self.s.recv(68)
+        print(data)
 t = Torrent("/home/andell/BitTorrrent_streaming/archlinux-2018.03.01-x86_64.iso.torrent")
 t.get_peer_addresses()
-print(t.peer_addresses)
+print(t.peer_addresses[0][0],t.peer_addresses[0][1])
+p = Peer(t.peer_addresses[0][0],t.peer_addresses[0][1], t)
+p.handshake()
